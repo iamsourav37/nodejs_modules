4448d2a10d25589dd74aa625fd0dd19fbc9403f7
iamsourav37 <souravganguly108@gmail.com>
2021-04-02 20:26:41 +0530

    listen

--- server/index.js
+++ server/index.js
@@ -4,4 +4,6 @@ http
   .createServer((req, res) => {
     res.end("hello world");
   })
-  .listen(3737);
+  .listen(3737, "127.0.0.1", () => {
+    console.log("listening to the port no 3737");
+  });
