79cd073403fd25fbbf4ec23a4ed41b9ed6a77ad8
iamsourav37 <souravganguly108@gmail.com>
2021-04-02 14:15:28 +0530

    make folder, create files and append data in files

--- asynchoronously.js
+++ asynchoronously.js
@@ -18,9 +18,27 @@ const fs = require("fs");
 
 // read file asynchronously
 fs.readFile("message.txt", "UTF-8", (err, data) => {
-  if (err) console.log(err);
-  else {
-    console.log("inside data : ", data);
-    dataInFile = data;
+  if (err) {
+    console.log("error : ", err);
+  } else {
+    console.log("Data :", data);
   }
+});
+
+// creating a folder
+
+// fs.mkdir("new_folder", (err) => {
+//   if (err) console.log(err);
+// });
+
+// create a file in that folder
+
+// fs.writeFile("new_folder/new.txt", "Hare Krishna", (err) => {
+//   if (err) {
+//     console.log("Error :", err);
+//   }
+// });
+
+fs.appendFile("new_folder/new.txt", " Hare Ram", (err) => {
+  if (err) console.log(err);
 });
