4acdfc01a2a84d168a5f0fdc75b76671afad7c71
iamsourav37 <souravganguly108@gmail.com>
2021-04-02 20:23:04 +0530

    node js http server

--- npm_module/index.js
+++ npm_module/index.js
@@ -2,9 +2,12 @@ const chalk = require("chalk");
 const validator = require("validator");
 
 console.log(chalk.bgBlackBright("Hello world!"));
-console.log(chalk.bgGreen("Hello") + chalk.bgRed(" World!"));
+console.log(chalk.bgGreen.inverse("Hello") + chalk.bgRed.inverse(" World!"));
 
-console.log(validator.isEmail("fo-)o@bar.com"));
+console.log(validator.isEmail("example@demo.com"));
 console.log(validator.isMobilePhone("601234567845", "en-IN"));
 console.log(validator.isPostalCode("123456", "IN"));
-console.log(validator.isStrongPassword("@hello_World_09"));
+console.log(
+  "Is password strong : ",
+  validator.isStrongPassword("@hello_World_")
+);
