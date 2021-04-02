777cdd8eb7c878dcf6000f2e3ba8af9eab37b5d0
iamsourav37 <souravganguly108@gmail.com>
2021-04-02 19:02:47 +0530

    validator module explore

--- npm_module/index.js
+++ npm_module/index.js
@@ -1,4 +1,10 @@
 const chalk = require("chalk");
+const validator = require("validator");
 
-console.log(chalk.yellow("Hello world!"));
-console.log(chalk.green("Hello") + chalk.red(" World!"));
+console.log(chalk.bgBlackBright("Hello world!"));
+console.log(chalk.bgGreen("Hello") + chalk.bgRed(" World!"));
+
+console.log(validator.isEmail("fo-)o@bar.com"));
+console.log(validator.isMobilePhone("601234567845", "en-IN"));
+console.log(validator.isPostalCode("123456", "IN"));
+console.log(validator.isStrongPassword("@hello_World_09"));
